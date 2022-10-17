493e8fd98e84437ea491293db06ab88e89d73332
mininekos <manuel2g2m@gmail.com>
2022-10-17 14:35:04 +0200

    V1.2 Metodos empezados

--- app/src/main/java/com/example/calculadora/Eventos/EventosBotones.java
+++ app/src/main/java/com/example/calculadora/Eventos/EventosBotones.java
@@ -1,8 +1,10 @@
 package com.example.calculadora.Eventos;
 
 import android.view.View;
+import android.widget.Button;
 
 import com.example.calculadora.MainActivity;
+import com.example.calculadora.R;
 import com.example.calculadora.databinding.ActivityMainBinding;
 
 public class EventosBotones implements View.OnClickListener {
@@ -21,6 +23,33 @@ public class EventosBotones implements View.OnClickListener {
 
     @Override
     public void onClick(View view) {
+        Button b = (Button) view;
+
+        if(b.getText().toString().matches("[0-9]")){
+            calculadora.agregarNumero(b.getText().toString());
+        }
+        if(b.getText().toString().matches("[-+X/]")){
+            calculadora.ageragarOperador(b.getText().toString());
+        }
+
+        switch(view.getId()) {
+            case R.id.btnMC:
+                    break;
+            case R.id.btnMmas:
+                    break;
+            case R.id.btnMmenos:
+                break;
+            case R.id.btnMR:
+                break;
+            case R.id.btnC:
+                break;
+            case R.id.btnMasMenos:
+                break;
+            case R.id.btnIgual:
+                break;
+            case R.id.btnPunto:
+                break;
+        }
 
     }
 }
--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -8,7 +8,10 @@ import com.example.calculadora.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    //Variables q necesite
+    //Variables que necesite
+    Double num;
+    StringBuilder cadena;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,4 +28,20 @@ public class MainActivity extends AppCompatActivity {
     //Metodos y lo que hacen los botones
 
     public ActivityMainBinding getBinding(){return binding;}
+
+    public void agregarNumero(String numero){
+        cadena.append(numero);
+    }
+
+    public void ageragarOperador(String operador) {
+        if(cadena.lastIndexOf("+")==cadena.length()-1)
+        cadena.append(operador);
+    }
+
+    public void calcular(){
+        Double num1, num2;
+        StringBuilder cadenaOperacion=cadena;
+        Boolean error=false;
+
+    }
 }
